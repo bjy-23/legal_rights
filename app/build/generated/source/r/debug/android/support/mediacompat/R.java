900197cda9e52173400957d19a757756c8f5f1bd
bjy <408273914@qq.com>
2018-02-26 13:55:38 +0800

    1

--- app/build/generated/source/r/debug/android/support/mediacompat/R.java
+++ app/build/generated/source/r/debug/android/support/mediacompat/R.java
@@ -90,13 +90,13 @@ public final class R {
         public static final int notification_background = 0x7f0800f7;
         public static final int notification_main_column = 0x7f0800f8;
         public static final int notification_main_column_container = 0x7f0800f9;
-        public static final int right_icon = 0x7f080120;
-        public static final int right_side = 0x7f080121;
-        public static final int status_bar_latest_event_content = 0x7f080152;
-        public static final int text = 0x7f080157;
-        public static final int text2 = 0x7f080158;
-        public static final int time = 0x7f08015f;
-        public static final int title = 0x7f080160;
+        public static final int right_icon = 0x7f08011a;
+        public static final int right_side = 0x7f08011b;
+        public static final int status_bar_latest_event_content = 0x7f08014c;
+        public static final int text = 0x7f080151;
+        public static final int text2 = 0x7f080152;
+        public static final int time = 0x7f080159;
+        public static final int title = 0x7f08015a;
     }
     public static final class integer {
         public static final int cancel_button_image_alpha = 0x7f090004;
--- app/build/generated/source/r/debug/android/support/transition/R.java
+++ app/build/generated/source/r/debug/android/support/transition/R.java
@@ -92,21 +92,21 @@ public final class R {
         public static final int notification_main_column = 0x7f0800f8;
         public static final int notification_main_column_container = 0x7f0800f9;
         public static final int parent_matrix = 0x7f080101;
-        public static final int right_icon = 0x7f080120;
-        public static final int right_side = 0x7f080121;
-        public static final int save_image_matrix = 0x7f080124;
-        public static final int save_non_transition_alpha = 0x7f080125;
-        public static final int save_scale_type = 0x7f080126;
-        public static final int status_bar_latest_event_content = 0x7f080152;
-        public static final int text = 0x7f080157;
-        public static final int text2 = 0x7f080158;
-        public static final int time = 0x7f08015f;
-        public static final int title = 0x7f080160;
-        public static final int transition_current_scene = 0x7f080168;
-        public static final int transition_layout_save = 0x7f080169;
-        public static final int transition_position = 0x7f08016a;
-        public static final int transition_scene_layoutid_cache = 0x7f08016b;
-        public static final int transition_transform = 0x7f08016c;
+        public static final int right_icon = 0x7f08011a;
+        public static final int right_side = 0x7f08011b;
+        public static final int save_image_matrix = 0x7f08011e;
+        public static final int save_non_transition_alpha = 0x7f08011f;
+        public static final int save_scale_type = 0x7f080120;
+        public static final int status_bar_latest_event_content = 0x7f08014c;
+        public static final int text = 0x7f080151;
+        public static final int text2 = 0x7f080152;
+        public static final int time = 0x7f080159;
+        public static final int title = 0x7f08015a;
+        public static final int transition_current_scene = 0x7f080162;
+        public static final int transition_layout_save = 0x7f080163;
+        public static final int transition_position = 0x7f080164;
+        public static final int transition_scene_layoutid_cache = 0x7f080165;
+        public static final int transition_transform = 0x7f080166;
     }
     public static final class integer {
         public static final int cancel_button_image_alpha = 0x7f090004;
